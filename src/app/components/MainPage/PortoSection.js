f77da85df9d3b3e838fa457f6e73845c7bbbe28a
deren21 <109866900+deren21@users.noreply.github.com>
2024-01-09 14:24:27 +0700

    <feat: add hero section>

---
+++ src/app/components/MainPage/PortoSection.js
@@ -0,0 +1,5 @@
+export default function PortoSection() {
+  return (
+    <section className="w-full min-h-screen relative bg-transparent  flex flex-row justify-center items-center"></section>
+  );
+}
