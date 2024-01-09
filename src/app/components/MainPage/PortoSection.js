978ff5c2d0e52d8cab9e0f4359d6c1efcb7ffc85
Hilmi Wismadi <dzakiwismadi@gmail.com>
2024-01-09 21:42:41 +0700

    feat: testing porto scrollable

--- src/app/components/MainPage/PortoSection.js
+++ src/app/components/MainPage/PortoSection.js
@@ -1,5 +1,23 @@
 export default function PortoSection() {
   return (
-    <section className="w-full min-h-screen relative bg-transparent  flex flex-row justify-center items-center"></section>
+    <section className="w-full min-h-screen relative bg-transparent  flex flex-row justify-center items-center bg-neutral-a">
+      <div className="w-[50vw] h-[50vh] bg-transparent border-4 border-red-600 rounded-md flex flex-col overflow-y-scroll  ">
+      <img
+        alt=""
+        src="/image/Porto/HeroEnfo.png"
+        className="w-full h-full"
+        draggable="false"
+      />
+      <img
+        alt=""
+        src="/image/Porto/AboutUsEnfo.png"
+        className="w-full h-full"
+        draggable="false"
+      />
+
+
+      </div>
+
+    </section>
   );
 }
