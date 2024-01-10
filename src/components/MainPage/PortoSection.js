4a8dbdbd9dfefdbff693b4850d1120ae249060aa
Hilmi Wismadi <dzakiwismadi@gmail.com>
2024-01-10 20:07:53 +0700

    feat: minor update

--- src/components/MainPage/PortoSection.js
+++ src/components/MainPage/PortoSection.js
@@ -2,10 +2,10 @@ export default function PortoSection() {
   return (
     <section className="w-full min-h-screen relative  flex flex-row justify-center items-center bg-white-MainPage">
       <div className="flex flex-col">
-        <div className="text-neutral-g text-[2.5vw] font-SourceSansProBold flex justify-center ">
+        <div className="text-neutral-g text-[2.3vw] font-SourceSansProBold flex justify-center ">
           Our Work in Action
         </div>
-        <div className="text-neutral-e w-[56vw] text-center text-[1vw] font-SourceSansProSemibold ">
+        <div className="text-neutral-e w-[58vw] text-center text-[1vw] font-SourceSansProSemibold ">
           Lorem ipsum dolor sit amet consectetur. Nunc luctus libero enim dui
           pellentesque dolor mattis lacus. Malesuada accumsan tempor ultrices
           id. Sed et id ut etiam. Et egestas varius at pellentesque nisl cursus
@@ -13,8 +13,8 @@ export default function PortoSection() {
           nunc leo eget natoque vitae. Urna ut vestibulum id nisl vel nunc.
           Massa scelerisque egestas egestas dictum ut velit feugiat.
         </div>
-        <div className="relative">
-          <div className="w-[50vw] h-[50vh] bg-transparent border-[0.2vw] border-[#E2E8F0] rounded-md flex flex-col overflow-y-scroll no-scrollbar  ">
+        <div className="relative py-[2vw]">
+          <div className="w-[50vw] h-[50vh] bg-transparent border-[1.2vw] border-[#E2E8F0] rounded-md flex flex-col overflow-y-scroll no-scrollbar  ">
             <img
               alt=""
               src="/image/Porto/HeroEnfo.png"
@@ -28,7 +28,13 @@ export default function PortoSection() {
               draggable="false"
             />
           </div>
-          <div className="absolute w-[14vw] h-[40vh] bg-black -bottom-[8vh] right-[0] flex flex-col overflow-y-scroll no-scrollbar  ">
+          <div className="absolute w-[14vw] h-[44vh] bg-black border-[1.2vw] border-[#E2E8F0] rounded-md -bottom-[4vh] right-[0] flex flex-col overflow-y-scroll no-scrollbar  ">
+            <img
+              alt=""
+              src="/image/Porto/HeroEnforianMobile.png"
+              className="w-full h-screen"
+              draggable="false"
+            />
             <img
               alt=""
               src="/image/Porto/HeroEnforianMobile.png"
