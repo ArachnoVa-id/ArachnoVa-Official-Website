dd5481072ccce644b2319027ffa3f0ebf19cc0f8
deren21 <109866900+deren21@users.noreply.github.com>
2024-01-22 12:38:32 +0700

    <feat: add about us page>

---
+++ src/components/AboutUs/SocialMediaIcon.jsx
@@ -0,0 +1,9 @@
+const SocialMediaIcon = ({ Icon }) => {
+  return (
+    <div className="w-[5.156vw] aspect-[1/1] rounded-full bg-[#1AB0C8] flex justify-center items-center">
+      {Icon}
+    </div>
+  );
+};
+
+export default SocialMediaIcon;
