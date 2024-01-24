af144a3c0c42d4a6ac4ba327fae45364d33547ff
deren21 <109866900+deren21@users.noreply.github.com>
2024-01-24 23:13:05 +0700

    <feat: add navbar mobile>

---
+++ src/components/AllPage/NavBar/NavItemMobile.jsx
@@ -0,0 +1,24 @@
+import Button from "../Button/Button";
+import NavItem from "./NavItem";
+
+const NavItemMobile = () => {
+  return (
+    <div
+      className="z-[110] absolute right-[9.535vw] top-[14.884vw] w-[27.395vw] h-[44.372vw] px-[3.023vw] gap-y-[5.581vw] flex flex-col justify-center items-center bg-[#F9FDFE] rounded-[2.326vw] border-[#E2E8F0]"
+      style={{ boxShadow: "0px 4px 20px -7px rgba(71, 85, 105, 0.10)" }}
+    >
+      <ul className="text-center block gap-y-[4.651vw]">
+        <NavItem Menu="Projects" href="/projects" />
+        <NavItem Menu="Services" href="/services" />
+        <NavItem Menu="About" href="/aboutus" />
+      </ul>
+      <button className="w-[21.395vw] h-[9.302vw] rounded-[1.86vw] flex items-center justify-center bg-gradient-to-br from-[#2EC1D9] to-[#179FB5] hover:translate-y-[-0.208vw] transition-all duration-500 ease-in-out">
+        <p className="font-InterBold text-[3.256vw] text-neutral-a">
+          Contact Us
+        </p>
+      </button>
+    </div>
+  );
+};
+
+export default NavItemMobile;
--- src/components/AllPage/NavBar/Navbar.jsx
+++ src/components/AllPage/NavBar/Navbar.jsx
@@ -1,13 +1,19 @@
+"use client";
+
 import Image from "next/image";
 import NavItem from "./NavItem";
 import Button from "../Button/Button";
+import { Twirl as Hamburger } from "hamburger-react";
+import { useState } from "react";
+import NavItemMobile from "./NavItemMobile";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <>
-      <nav className="fixed z-[100] py-[0.938vw] px-[15vw] w-screen flex items-center justify-between bg-transparent backdrop-blur-[0.208vw] border-b-[0.104vw] border-[#E6ECF0]">
+      <nav className="relative lg:fixed z-[100] lg:py-[0.938vw] lg:px-[15vw] px-[5.581vw] py-[2.88vw] w-screen flex items-center justify-between lg:bg-transparent bg-[#FAFDFF] lg:backdrop-blur-[0.208vw] backdrop-blur-[1.163vw] lg:border-b-[0.104vw] border-b-[0.465vw] border-[#E6ECF0]">
         <div className="flex items-center flex-row">
-          <ul className="flex items-center gap-x-[3.125vw]">
+          <ul className="lg:flex items-center gap-x-[3.125vw] hidden">
             <a href="/">
               <img
                 className="w-[3.281vw] aspect-[69/53] hover:scale-110 transition-all duration-300 ease-in-out"
@@ -21,7 +27,30 @@ const Navbar = () => {
             <NavItem Menu="Services" href="/services" />
             <NavItem Menu="About" href="/aboutus" />
           </ul>
+          <a href="/">
+            <img
+              className="lg:hidden w-[10.465vw] aspect-[45/33] hover:scale-110 transition-all duration-300 ease-in-out"
+              src="/image/Logo.png"
+              draggable="false"
+              alt="logo"
+            />
+          </a>
         </div>
+        <div className="w-fit h-fit block lg:hidden">
+          <Hamburger
+            toggled={isOpen}
+            toggle={setIsOpen}
+            color="#1AB0C8"
+            size={22}
+          />
+        </div>
+        {isOpen ? (
+          <>
+            <NavItemMobile />
+          </>
+        ) : (
+          <></>
+        )}
         <Button Text="Contact Us" />
       </nav>
     </>
