ea35da93b57c03eeb0cdae81f3443e8dfcbb5639
Hilmi Wismadi <dzakiwismadi@gmail.com>
2024-01-25 19:26:46 +0700

    feat: projects section foundation

--- src/components/MainPage/PortoSection/PortoSection.js
+++ src/components/MainPage/PortoSection/PortoSection.js
@@ -5,7 +5,8 @@ import { FaArrowRight } from "react-icons/fa";
 export default function PortoSection() {
   return (
     <section className="w-full min-h-screen relative  flex flex-row justify-center items-center bg-white-MainPage gap-y-[2.083vw]">
-      <div className="flex flex-col justify-center items-center">
+      {/* Desktop */}
+      <div className="flex flex-col justify-center items-center max-lg:hidden">
         <div className="text-neutral-g text-[2.3vw] font-SourceSansProBold flex justify-center ">
           Our Work in Action
         </div>
@@ -66,6 +67,57 @@ export default function PortoSection() {
           <FaArrowRight className="text-[#24AAA7] w-[1.25vw] aspect-[1/1]" />
         </button>
       </div>
+
+      {/* Mobile */}
+      <div className="flex flex-col justify-center items-center lg:hidden">
+        <div className="text-neutral-g text-[8vw] font-SourceSansProBold flex justify-center ">
+          Our Work in Action
+        </div>
+        <div className="text-neutral-e w-[78vw] text-center text-[2.5vw] font-SourceSansProSemibold my-[1vw] ">
+          Lorem ipsum dolor sit amet consectetur. Nunc luctus libero enim dui
+          pellentesque dolor mattis lacus. Malesuada accumsan tempor ultrices
+          id. Sed et id ut etiam. Et egestas varius at pellentesque nisl cursus
+          donec arcu. Nisi elementum lectus imperdiet elit risus. Mi at orci
+          nunc leo eget natoque vitae. Urna ut vestibulum id nisl vel nunc.
+          Massa scelerisque egestas egestas dictum ut velit feugiat.
+        </div>
+        <div className="w-[80vw] h-[45vw] mt-[2vw] bg-transparent border-[2.2vw] border-[#E2E8F0]  rounded-md flex flex-col overflow-y-scroll no-scrollbar  ">
+          <img
+            alt=""
+            src="/image/Porto/HeroEnfo.png"
+            className="w-full h-screen"
+            draggable="false"
+          />
+          <img
+            alt=""
+            src="/image/Porto/AboutUsEnfo.png"
+            className="w-full h-screen"
+            draggable="false"
+          />
+        </div>
+
+        <div className="w-[40vw] h-[70vw] mt-[3vw] bg-black border-[2.2vw] border-[#E2E8F0] rounded-md -bottom-[4vh] -right-[4vw] flex flex-col overflow-y-scroll no-scrollbar  ">
+          <img
+            alt=""
+            src="/image/Porto/HeroEnforianMobile.png"
+            className="w-full h-screen"
+            draggable="false"
+          />
+          <img
+            alt=""
+            src="/image/Porto/TeaserEnfo.png"
+            className="w-full h-screen"
+            draggable="false"
+          />
+          <img
+            alt=""
+            src="/image/Porto/AboutUsEnfoMobile.png"
+            className="w-full h-screen"
+            draggable="false"
+          />
+        </div>
+      </div>
+
       <BorderSeparator />
     </section>
   );
