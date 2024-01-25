e87461a06b320d53b03c8166d98e42fd88de9542
deren21 <109866900+deren21@users.noreply.github.com>
2024-01-25 13:03:34 +0700

    <fix: fix nav list mobile>

--- src/components/AllPage/NavBar/NavItemMobile.jsx
+++ src/components/AllPage/NavBar/NavItemMobile.jsx
@@ -7,7 +7,7 @@ const NavItemMobile = () => {
       className="z-[110] absolute right-[9.535vw] top-[14.884vw] w-[27.395vw] h-[44.372vw] px-[3.023vw] gap-y-[5.581vw] flex flex-col justify-center items-center bg-[#F9FDFE] rounded-[2.326vw] border-[#E2E8F0]"
       style={{ boxShadow: "0px 4px 20px -7px rgba(71, 85, 105, 0.10)" }}
     >
-      <ul className="text-center block gap-y-[4.651vw]">
+      <ul className="text-center block leading-[7vw]">
         <NavItem Menu="Projects" href="/projects" />
         <NavItem Menu="Services" href="/services" />
         <NavItem Menu="About" href="/aboutus" />
