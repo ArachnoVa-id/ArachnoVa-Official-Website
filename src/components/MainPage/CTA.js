a0a1a414f40d0e31f7f333e0cea055f5820c0b26
Hilmi Wismadi <dzakiwismadi@gmail.com>
2024-01-25 19:36:07 +0700

    feat: CTA Mobile

--- src/components/MainPage/CTA.js
+++ src/components/MainPage/CTA.js
@@ -1,13 +1,15 @@
 export default function CTA() {
   return (
-    <section className="w-full h-[60vh] relative bg-transparent flex flex-row justify-center items-center">
+    <section className="w-full lg:h-[60vh] max-lg:h-[30vh] relative bg-transparent flex flex-row justify-center items-center">
       <img
         alt=""
         src="/image/CTA/BGCTA.png"
         className="absolute w-full h-full -z-[1]  "
         draggable="false"
       />
-      <div className="flex flex-col justify-center items-center">
+
+      {/* Desktop */}
+      <div className="flex flex-col justify-center items-center max-lg:hidden">
         <div className="text-neutral-g font-SourceSansProBold text-[3vw]/[3vw] flex flex-col justify-center items-center">
           <p>Ready to Transform Your Digital Presence ? </p>
           <p>Let's Start Crafting Your Website Today</p>
@@ -16,6 +18,18 @@ export default function CTA() {
           Start Your Project
         </button>
       </div>
+
+      {/* Mobile */}
+      <div className="flex flex-col justify-center items-center lg:hidden">
+        <div className="text-neutral-g font-SourceSansProBold text-[4.5vw]/[5vw] flex flex-col justify-center items-center">
+          <p>Ready to Transform Your Digital Presence ? </p>
+          <p>Let's Start Crafting Your Website Today</p>
+        </div>
+        <button className="bg-black w-[60vw] h-[8vw] rounded-lg font-SourceSansProSemibold mt-[3vw] text-[4.5vw] hover:bg-neutral-e duration-500">
+          Start Your Project
+        </button>
+      </div>
+
     </section>
   );
 }
