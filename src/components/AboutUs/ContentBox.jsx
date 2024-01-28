8b8ef9837ff5f741dec87c176ccbcfa930cd9a48
deren21 <109866900+deren21@users.noreply.github.com>
2024-01-28 15:01:43 +0700

    <feat: add responsive to about us page>

--- src/components/AboutUs/ContentBox.jsx
+++ src/components/AboutUs/ContentBox.jsx
@@ -1,15 +1,15 @@
 const ContentBox = ({ Title, Description, Background, Border }) => {
   return (
     <div
-      className={`w-[31.667vw] aspect-[608/316] flex flex-col justify-center items-center rounded-[0.833vw] border-[0.156vw] ${Border}`}
+      className={`lg:w-[31.667vw] lg:aspect-[608/316] w-[90.698vw] aspect-[390/234] flex flex-col justify-center items-center lg:rounded-[0.833vw] rounded-[3.721vw] lg:border-[0.156vw] border-[0.698vw] ${Border}`}
       style={{
         background: Background,
       }}
     >
-      <h1 className="font-SourceSansProBold text-[2.083vw] text-neutral-g">
+      <h1 className="font-SourceSansProBold lg:text-[2.083vw] text-[6.047vw] text-neutral-g">
         Our <span className="text-[#0893D4]">{Title}</span>
       </h1>
-      <p className="w-[25.938vw] font-SourceSansProSemibold text-[1.042vw] text-neutral-e">
+      <p className="lg:w-[25.938vw] w-[79.535vw] font-SourceSansProSemibold lg:text-[1.042vw] text-[4.186vw] text-neutral-e text-justify">
         {Description}
       </p>
     </div>
--- src/components/AboutUs/SocialMediaIcon.jsx
+++ src/components/AboutUs/SocialMediaIcon.jsx
@@ -1,6 +1,6 @@
 const SocialMediaIcon = ({ Icon }) => {
   return (
-    <div className="w-[5.156vw] aspect-[1/1] rounded-full bg-[#1AB0C8] flex justify-center items-center">
+    <div className="lg:w-[5.156vw] w-[15.116vw] aspect-[1/1] rounded-full bg-[#1AB0C8] flex justify-center items-center">
       {Icon}
     </div>
   );
