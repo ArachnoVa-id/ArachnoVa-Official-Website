eb30a817136b026c4cbdccf1927f34c9be278b30
deren21 <109866900+deren21@users.noreply.github.com>
2024-01-31 21:14:27 +0700

    <feat: add href variable>

--- src/components/AllPage/Button/Button.jsx
+++ src/components/AllPage/Button/Button.jsx
@@ -1,9 +1,12 @@
-const Button = ({ Text }) => {
-  return (
-    <button className="lg:w-[5.677vw] lg:aspect-[109/40] lg:rounded-[0.417vw] lg:flex hidden items-center justify-center bg-gradient-to-br from-[#2EC1D9] to-[#179FB5] hover:translate-y-[-0.208vw] transition-all duration-500 ease-in-out">
-      <p className="font-InterBold text-[0.729vw] text-neutral-a">{Text}</p>
-    </button>
-  );
+const Button = ({ Text, href }) => {
+	return (
+		<a
+			href={href}
+			className="lg:w-[5.677vw] lg:aspect-[109/40] lg:rounded-[0.417vw] lg:flex hidden items-center justify-center bg-gradient-to-br from-[#2EC1D9] to-[#179FB5] hover:translate-y-[-0.208vw] transition-all duration-500 ease-in-out"
+		>
+			<p className="font-InterBold text-[0.729vw] text-neutral-a">{Text}</p>
+		</a>
+	);
 };
 
 export default Button;
--- src/components/AllPage/NavBar/Navbar.jsx
+++ src/components/AllPage/NavBar/Navbar.jsx
@@ -8,53 +8,53 @@ import { useState } from "react";
 import NavItemMobile from "./NavItemMobile";
 
 const Navbar = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  return (
-    <>
-      <nav className="relative lg:fixed z-[100] lg:py-[0.938vw] lg:px-[15vw] px-[5.581vw] py-[2.88vw] w-screen flex items-center justify-between lg:bg-transparent bg-[#FAFDFF] lg:backdrop-blur-[0.208vw] backdrop-blur-[1.163vw] lg:border-b-[0.104vw] border-b-[0.465vw] border-[#E6ECF0]">
-        <div className="flex items-center flex-row">
-          <ul className="lg:flex items-center gap-x-[3.125vw] hidden">
-            <a href="/">
-              <img
-                className="w-[3.281vw] aspect-[69/53] hover:scale-110 transition-all duration-300 ease-in-out"
-                src="/image/Logo.png"
-                draggable="false"
-                alt="logo"
-              />
-            </a>
+	const [isOpen, setIsOpen] = useState(false);
+	return (
+		<>
+			<nav className="relative lg:fixed z-[100] lg:py-[0.938vw] lg:px-[15vw] px-[5.581vw] py-[2.88vw] w-screen flex items-center justify-between lg:bg-transparent bg-[#FAFDFF] lg:backdrop-blur-[0.208vw] backdrop-blur-[1.163vw] lg:border-b-[0.104vw] border-b-[0.465vw] border-[#E6ECF0]">
+				<div className="flex items-center flex-row">
+					<ul className="lg:flex items-center gap-x-[3.125vw] hidden">
+						<a href="/">
+							<img
+								className="w-[3.281vw] aspect-[69/53] hover:scale-110 transition-all duration-300 ease-in-out"
+								src="/image/Logo.png"
+								draggable="false"
+								alt="logo"
+							/>
+						</a>
 
-            <NavItem Menu="Projects" href="/projects" />
-            <NavItem Menu="Services" href="/services" />
-            <NavItem Menu="About" href="/aboutus" />
-          </ul>
-          <a href="/">
-            <img
-              className="lg:hidden w-[10.465vw] aspect-[45/33] hover:scale-110 transition-all duration-300 ease-in-out"
-              src="/image/Logo.png"
-              draggable="false"
-              alt="logo"
-            />
-          </a>
-        </div>
-        <div className="w-fit h-fit block lg:hidden">
-          <Hamburger
-            toggled={isOpen}
-            toggle={setIsOpen}
-            color="#1AB0C8"
-            size={22}
-          />
-        </div>
-        {isOpen ? (
-          <>
-            <NavItemMobile />
-          </>
-        ) : (
-          <></>
-        )}
-        <Button Text="Contact Us" />
-      </nav>
-    </>
-  );
+						<NavItem Menu="Projects" href="/projects" />
+						<NavItem Menu="Services" href="/services" />
+						<NavItem Menu="About" href="/aboutus" />
+					</ul>
+					<a href="/">
+						<img
+							className="lg:hidden w-[10.465vw] aspect-[45/33] hover:scale-110 transition-all duration-300 ease-in-out"
+							src="/image/Logo.png"
+							draggable="false"
+							alt="logo"
+						/>
+					</a>
+				</div>
+				<div className="w-fit h-fit block lg:hidden">
+					<Hamburger
+						toggled={isOpen}
+						toggle={setIsOpen}
+						color="#1AB0C8"
+						size={22}
+					/>
+				</div>
+				{isOpen ? (
+					<>
+						<NavItemMobile />
+					</>
+				) : (
+					<></>
+				)}
+				<Button Text="Contact Us" href="#" />
+			</nav>
+		</>
+	);
 };
 
 export default Navbar;
