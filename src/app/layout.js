969286e5f0fe7b7eede9fee0be04eabe3995c50b
deren21 <109866900+deren21@users.noreply.github.com>
2024-02-04 21:22:22 +0700

    <feat: add smooth scroll>

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,18 +6,18 @@ import Footer from "@/components/AllPage/Footer/Footer";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "ArachnoVa",
-  description: "ArachnoVa Official Website",
+	title: "ArachnoVa",
+	description: "ArachnoVa Official Website",
 };
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body>
-        <Navbar />
-        <div className="flex flex-col">{children}</div>
-        <Footer />
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en" className="scroll-smooth">
+			<body>
+				<Navbar />
+				<div className="flex flex-col">{children}</div>
+				<Footer />
+			</body>
+		</html>
+	);
 }
--- src/components/Services/ServicesProfiling/ServicesProfiling.jsx
+++ src/components/Services/ServicesProfiling/ServicesProfiling.jsx
@@ -2,26 +2,27 @@ import Content from "@/components/MainPage/OurProducts/Content";
 import ServicesContainer from "../ServicesContainer";
 
 const ServicesProfiling = () => {
-  return (
-    <div
-      id="ServicesProfilingPage"
-      className="w-full lg:min-h-[39.531vw] min-h-[175.349vw] flex flex-col lg:justify-start justify-center items-center bg-white-MainPage"
-    >
-      <ServicesContainer
-        Title="Profiling Page"
-        Description="Lorem ipsum dolor sit amet consectetur. Praesent phasellus sagittis dignissim id egestas aenean dictum et. Gravida enim rutrum adipiscing adipiscing sed vitae."
-        Function="Lorem ipsum dolor sit amet consectetur. Iaculis."
-        Duration="Lorem ipsum dolor sit amet consectetur. Iaculis."
-        Pages="Lorem ipsum dolor sit amet consectetur. Iaculis."
-        SlideContent={
-          <Content
-            data={["/image/OurProducts/Profiling-1.png"]}
-            totalSlide={1}
-          />
-        }
-      />
-    </div>
-  );
+	return (
+		<div
+			id="ServicesProfilingPage"
+			className="w-full lg:min-h-[39.531vw] min-h-[175.349vw] flex flex-col lg:justify-start justify-center items-center bg-white-MainPage"
+		>
+			<ServicesContainer
+				Title="Profiling Page"
+				Description="Lorem ipsum dolor sit amet consectetur. Praesent phasellus sagittis dignissim id egestas aenean dictum et. Gravida enim rutrum adipiscing adipiscing sed vitae."
+				Function="Lorem ipsum dolor sit amet consectetur. Iaculis."
+				Duration="Lorem ipsum dolor sit amet consectetur. Iaculis."
+				Pages="Lorem ipsum dolor sit amet consectetur. Iaculis."
+				SlideContent={
+					<Content
+						data={["/image/OurProducts/Profiling-1.png"]}
+						totalSlide={1}
+					/>
+				}
+				Variant="left"
+			/>
+		</div>
+	);
 };
 
 export default ServicesProfiling;
