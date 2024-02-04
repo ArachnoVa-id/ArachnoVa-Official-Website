205042dc0b2cfafcd5888168820178df75d8180c
deren21 <109866900+deren21@users.noreply.github.com>
2024-02-04 22:16:23 +0700

    <fix: services page>

--- src/components/Services/ServicesCustom/ServicesCustom.jsx
+++ src/components/Services/ServicesCustom/ServicesCustom.jsx
@@ -2,26 +2,42 @@ import Content from "@/components/MainPage/OurProducts/Content";
 import ServicesContainer from "../ServicesContainer";
 
 const ServicesCustomWeb = () => {
-  return (
-    <div
-      id="ServicesCustomWeb"
-      className="w-full lg:min-h-[39.531vw] min-h-[175.349vw] flex flex-col lg:justify-start justify-center items-center bg-white-MainPage"
-    >
-      <ServicesContainer
-        Title="Custom Web"
-        Description="Lorem ipsum dolor sit amet consectetur. Praesent phasellus sagittis dignissim id egestas aenean dictum et. Gravida enim rutrum adipiscing adipiscing sed vitae."
-        Function="Lorem ipsum dolor sit amet consectetur. Iaculis."
-        Duration="Lorem ipsum dolor sit amet consectetur. Iaculis."
-        Pages="Lorem ipsum dolor sit amet consectetur. Iaculis."
-        SlideContent={
-          <Content
-            data={["/image/OurProducts/CustomWeb-1.png"]}
-            totalSlide={1}
-          />
-        }
-      />
-    </div>
-  );
+	const Description =
+		"Lorem ipsum dolor sit amet consectetur. Praesent phasellus sagittis dignissim id egestas aenean dictum et. Gravida enim rutrum adipiscing adipiscing sed vitae.";
+	const Function = "Lorem ipsum dolor sit amet consectetur. Iaculis.";
+	const Duration = "Lorem ipsum dolor sit amet consectetur. Iaculis.";
+	const Pages = "Lorem ipsum dolor sit amet consectetur. Iaculis.";
+	const Data = ["/image/OurProducts/CustomWeb-1.png"];
+	const TotalSlide = 1;
+	return (
+		<section
+			id="ServicesCustomWeb"
+			className="w-full lg:min-h-[39.531vw] min-h-[175.349vw] flex flex-col lg:justify-start justify-center items-center bg-white-MainPage"
+		>
+			<div className="lg:flex hidden">
+				<ServicesContainer
+					Title="Custom Web"
+					Description={Description}
+					Function={Function}
+					Duration={Duration}
+					Pages={Pages}
+					SlideContent={<Content data={Data} totalSlide={TotalSlide - 1} />}
+				/>
+			</div>
+			<div className="lg:hidden flex">
+				<ServicesContainer
+					Title="Custom Web"
+					Description={Description}
+					Function={Function}
+					Duration={Duration}
+					Pages={Pages}
+					SlideContent={
+						<Content data={Data} totalSlide={TotalSlide - 1} variant="Mobile" />
+					}
+				/>
+			</div>
+		</section>
+	);
 };
 
 export default ServicesCustomWeb;
