f7c8fd1ea7f96a0d8a4cd803967b25582f957806
deren21 <109866900+deren21@users.noreply.github.com>
2024-02-05 22:21:42 +0700

    <feat: add hover animation>

--- src/components/MainPage/OurMechs/Container.jsx
+++ src/components/MainPage/OurMechs/Container.jsx
@@ -6,13 +6,13 @@ const Container = ({ Icon, Title, Description, State, Clicked }) => {
 		: "bg-transparent";
 	return (
 		<div
-			className={`lg:w-[32.917vw] lg:h-[6.875vw] w-[90.698vw] h-[29.07vw] flex flex-row lg:p-[1.25vw] gap-x-[1.042vw] rounded-[0.729vw] lg:cursor-pointer z-50 ${style}}`}
+			className={`group lg:w-[32.917vw] lg:h-[6.875vw] w-[90.698vw] h-[29.07vw] flex flex-row lg:p-[1.25vw] gap-x-[1.042vw] rounded-[0.729vw] lg:cursor-pointer z-50 ${style}}`}
 			onClick={Clicked}
 		>
 			<div className="w-fit h-fit lg:flex hidden">{Icon}</div>
 			<div className="flex flex-col gap-y-[0.469vw]">
 				<div className="w-fit h-fit flex lg:hidden">{Icon}</div>
-				<p className="font-SourceSansProBold lg:text-[1.458vw] text-[4.651vw]/[6.977vw] text-neutral-g lg:leading-[1.563vw]">
+				<p className="lg:group-hover:bg-clip-text lg:group-hover:text-transparent lg:group-hover:bg-gradient-to-r lg:group-hover:from-[#0893D4] lg:group-hover:via-[#44C4D9] lg:group-hover:to-[#159E8D] transition-all duration-500 ease-in-out font-SourceSansProBold lg:text-[1.458vw] text-[4.651vw]/[6.977vw] text-neutral-g lg:leading-[1.563vw]">
 					{Title}
 				</p>
 				<p className="font-SourceSansProSemibold lg:text-[0.833vw] text-[3.721vw] lg:leading-normal leading-[4.651vw] text-[#64748B]">
