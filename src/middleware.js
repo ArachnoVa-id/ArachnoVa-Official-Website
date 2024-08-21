cfd4d205412d66629d9860e27fc6aafae559558f
Yitzhak Edmund Tio Manalu <121650790+iZcy@users.noreply.github.com>
2024-08-21 23:29:10 +0700

    feat: cancel rollback

--- src/middleware.js
+++ src/middleware.js
@@ -1,11 +1,10 @@
 import { NextResponse } from 'next/server'
  
 export function middleware(request) {
-  // if (request.nextUrl.pathname.startsWith('/academy/booklet')) {
-  //   return NextResponse.rewrite(new URL('https://www.figma.com/slides/AdcmoUcleIzn8gtGFtvEYK/Booklet-ArachnoVa-Academy-(BasPro)?node-id=2-439&t=e8wXSkLiHTiR4HfQ-1', request.url))
-  // }
-  // else
-  if (request.nextUrl.pathname.startsWith('/academy')) {
+  if (request.nextUrl.pathname.startsWith('/academy/booklet')) {
+    return NextResponse.rewrite(new URL('https://www.figma.com/slides/AdcmoUcleIzn8gtGFtvEYK/Booklet-ArachnoVa-Academy-(BasPro)?node-id=2-439&t=e8wXSkLiHTiR4HfQ-1', request.url))
+  }
+  else if (request.nextUrl.pathname.startsWith('/academy')) {
     return NextResponse.rewrite(new URL('https://forms.gle/47BD69dSta8wDPrV8', request.url))
   }
 }
