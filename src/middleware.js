ee4c792c7d1b7abfadfea220602c6cc17af0531a
iZcy <yitzhakedmundtiomanalu@mail.ugm.ac.id>
2024-08-22 08:17:32 +0700

    fix: middleware load & image load

--- src/middleware.js
+++ src/middleware.js
@@ -1,10 +1,15 @@
-import { NextResponse } from 'next/server'
- 
+import { NextResponse } from "next/server";
+
 export function middleware(request) {
-  if (request.nextUrl.pathname.startsWith('/academy/booklet')) {
-    return NextResponse.rewrite(new URL('https://www.figma.com/slides/AdcmoUcleIzn8gtGFtvEYK/Booklet-ArachnoVa-Academy-(BasPro)?node-id=2-439&t=e8wXSkLiHTiR4HfQ-1', request.url))
+  const { pathname } = request.nextUrl;
+
+  if (pathname.startsWith("/academy/booklet")) {
+    return NextResponse.redirect(
+      "https://www.figma.com/slides/AdcmoUcleIzn8gtGFtvEYK/Booklet-ArachnoVa-Academy-(BasPro)?node-id=2-439&t=e8wXSkLiHTiR4HfQ-1"
+    );
+  } else if (pathname.startsWith("/academy")) {
+    return NextResponse.redirect("https://forms.gle/47BD69dSta8wDPrV8");
   }
-  else if (request.nextUrl.pathname.startsWith('/academy')) {
-    return NextResponse.rewrite(new URL('https://forms.gle/47BD69dSta8wDPrV8', request.url))
-  }
+
+  return NextResponse.next();
 }
