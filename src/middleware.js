f2db852913f3e8ed74ee4a8ad8ac5f7a5262c316
iZcy <yitzhakedmundtiomanalu@mail.ugm.ac.id>
2024-08-22 08:18:23 +0700

    feat: equal

--- src/middleware.js
+++ src/middleware.js
@@ -3,11 +3,11 @@ import { NextResponse } from "next/server";
 export function middleware(request) {
   const { pathname } = request.nextUrl;
 
-  if (pathname.startsWith("/academy/booklet")) {
+  if (pathname === "/academy/booklet") {
     return NextResponse.redirect(
       "https://www.figma.com/slides/AdcmoUcleIzn8gtGFtvEYK/Booklet-ArachnoVa-Academy-(BasPro)?node-id=2-439&t=e8wXSkLiHTiR4HfQ-1"
     );
-  } else if (pathname.startsWith("/academy")) {
+  } else if (pathname === "/academy") {
     return NextResponse.redirect("https://forms.gle/47BD69dSta8wDPrV8");
   }
 
